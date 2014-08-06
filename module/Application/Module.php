0818424b1c1a1e70f6c559d79d7d5ce805a8a55a
eddylecca <eddy.lecca3@gmail.com>
2014-08-05 20:01:37 -0500

    12.- Alias de Url Dinámico

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -24,6 +24,11 @@ use Zend\Db\ResultSet\ResultSet;
 use Zend\Db\TableGateway\TableGateway;
 use Zend\Authentication\AuthenticationService;
 
+//.. Primero cargamos las clases de Acl de Zen
+use Zend\Permissions\Acl\Acl;
+use Zend\Permissions\Acl\Role\GenericRole;
+use Zend\Permissions\Acl\Resource\GenericResource;
+
 class Module implements AutoloaderProviderInterface, ConfigProviderInterface {
 
     /*
